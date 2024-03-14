eeb1944aee1d5862e30789edd9bab7af8f8884cd
hieuanh <142485796+anhhieuneu@users.noreply.github.com>
2024-03-14 07:55:30 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,25 @@
-public class MP3 {
+public class MP3 extends item {
+    private int duration;
+
+    public MP3(String ten, String description, String ID, float gia, int duration) {
+        super(ten, description, ID, gia);
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Name: " + ten);
+        System.out.println("Description: " + getDescription());
+        System.out.println("ID: " + ID);
+        System.out.println("Price: " + gia);
+        System.out.println("Duration: " + duration);
+    }
 }

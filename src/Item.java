630a1568fff78c90545c3bb5188c92079304b6aa
hieuanh <142485796+anhhieuneu@users.noreply.github.com>
2024-03-14 07:54:59 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,24 @@
-public class Item {
+public abstract class item {
+    public String ten;
+    private String description;
+    protected String ID;
+    float gia;
+
+    public item(String ten, String description, String ID, float gia) {
+        this.ten = ten;
+        this.description = description;
+        this.ID = ID;
+        this.gia = gia;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public abstract void showInfo();
 }
+

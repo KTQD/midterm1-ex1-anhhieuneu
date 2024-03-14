38d04bdabf4d364b73754920b59ddda215aa54fd
hieuanh <142485796+anhhieuneu@users.noreply.github.com>
2024-03-14 07:54:11 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
-        MP3 mp3;
+        MP3 mp3 = new MP3("thien ly", "jack music", "MP3-001", 0.0f, 240);
+        book book = new book("harry pot", "magic", "BôK-001", 40.0f, "ko nho", 400, "chuyen dai");
+        System.out.println("MP3:");
         mp3.showInfo();
-
-        Book book;
+        System.out.println();
+        System.out.println("book:");
         book.showInfo();
-
     }
 }
